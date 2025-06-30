e593b8edf77e23de21d7a93e4885d0d5339618d0
Bhavya Shah <122201358+Bhavya3604@users.noreply.github.com>
2025-06-30 19:45:34 +0000

    database set up

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,17 +1,13 @@
-
 import app from './app.js';
 import { connectToDatabase } from './db/connection.js';
 //connections and listeners
 const PORT = process.env.PORT || 5000;
 connectToDatabase()
-  .then(() => {
+    .then(() => {
     console.log("Connected to Database");
-    app.listen(PORT,() => 
-      console.log(`Server is running on port ${PORT}`));
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 })
-.catch((error) => {
-  console.log(error);
+    .catch((error) => {
+    console.log(error);
 });
-
-
-
+//# sourceMappingURL=index.js.map
